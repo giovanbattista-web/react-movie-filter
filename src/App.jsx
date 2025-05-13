643b019c6a5feb85d861772ab4dad4d53b847f40
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-13 09:18:05 +0200

    [UPD] ULTIMA PARTE

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,32 @@ const films = [
 ]
 
 function App() {
+  const [genre, setGenre] = useState("");
+  const [filteredFilms, setFilteredFilms] = useState(films);
+
+  useEffect(() => {
+    console.log("updated");
+    if (genre === "") { setFilteredFilms(films) }
+    else {
+      const selectedFilms = films.filter((films) =>
+        films.genre === genre);
+
+      setFilteredFilms(selectedFilms);
+    }
+  }, [genre]);
+
+  function handleIncrement() {
+    setGenre((current) => current + 1);
+  }
+
   return (
     <>
       <div className='container'>
         <div className='row gy-4'>
           <div className='col-12'>
             <h1>FILM</h1>
-            <select class="form-select" aria-label="Default select example">
-              <option selected>Open this select menu</option>
+            <select value={genre} onChange={(e) => { setGenre(e.target.value) }} className="form-select" aria-label="Default select example">
+              <option value="">Open this select menu</option>
               <option value="1">Fantascienza</option>
               <option value="2">Thriller</option>
               <option value="3">Romantico</option>
@@ -26,18 +44,14 @@ function App() {
           </div>
           <div className='col-12'>
             <ul className='list-group'>
-              {films.map((film, index) => (
+              {filteredFilms.map((films, index) => (
                 <li className='list-group-item' key={index}>
                   <div>
-                    <div>{film.title}</div>
+                    <div>{films.title}</div>
                   </div>
                 </li>
               ))
-
-
-
               }
-
             </ul>
           </div>
         </div>
