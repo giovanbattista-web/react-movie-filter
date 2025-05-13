84338610e37055e3192167173c96219879fc5a2f
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-13 06:50:52 +0200

    [UPD] LISTA DEI FILM MOSTRATA IN PAGINA

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-[
+const films = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -10,12 +10,34 @@ import { useState, useEffect } from 'react';
 ]
 
 function App() {
-
   return (
     <>
+      <div className='container'>
+        <div className='row gy-4'>
+          <div className='col-12'>
+            <h1>FILM</h1>
+          </div>
+          <div className='col-12'>
+            <ul className='list-group'>
+              {films.map((film, index) => (
+                <li className='list-group-item' key={index}>
+                  <div>
+                    <div>{film.title}</div>
+                  </div>
+                </li>
+              ))
 
+
+
+              }
+
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
+
 };
 
 export default App;
