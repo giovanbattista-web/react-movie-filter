e81fc45fb7df85e9a88566b4b1d7c9bcdcc090dd
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-13 05:11:30 +0200

    [UPD] LISTA DEI FILM CARICATA

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import { useState, useEffect } from 'react';
 
+[
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
 function App() {
 
   return (
